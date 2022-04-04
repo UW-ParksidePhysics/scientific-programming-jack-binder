340216855cfc8eb73d680a0395201ea541e023bd
jack-binder <binde009@rangers.uwp.edu>
2022-04-04 16:20:25 -0500

    changed file

---
+++ matlab/Thermal_energy.m
@@ -0,0 +1,16 @@
+%import data file into code (put them into matrix)
+%create functions
+%create graph with radii on x axis and thermal energy on y axis
+%Solving for current formula: i = integral of Jda
+%Solving for thermal energy formula: E = Vit
+current = [1];
+voltage = 2; % (V)
+time = 120; %(sec)
+function current_matrix = current(current_desnity*differential_area)
+    for 
+end
+energy = voltage*current*time;
+for 1.0:1.0
+
+
+
