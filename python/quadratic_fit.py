745658ebd3c01ebdf8c527081a9a9c418683609a
jack-binder <binde009@rangers.uwp.edu>
2022-05-09 14:55:57 -0500

    Updated Project

--- python/quadratic_fit.py
+++ python/quadratic_fit.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def bivariate_statistics(data):
+def quadratic_fit(data):
     xs = data[:, 0]
     ys = data[:, 1]
     quadratic_coefficients = np.polyfit(xs, ys, 2)
@@ -10,7 +10,7 @@ def bivariate_statistics(data):
 
 if __name__ == "__main__":
     test_data = np.array([[0, 0], [1, 1], [2, 4]])
-
-    coefficients = bivariate_statistics(test_data)
+    coefficients = quadratic_fit(test_data)
     print(coefficients)
+
 
