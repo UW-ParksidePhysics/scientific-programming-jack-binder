596514bf7a96ec5690be8be46687ee16478eb02d
jack-binder <binde009@rangers.uwp.edu>
2022-05-09 15:21:57 -0500

    Updated Project

--- python/fit_curve_array.py
+++ python/fit_curve_array.py
@@ -1,7 +1,22 @@
+__author__ = "Pancho Rodriquez"
 import numpy as np
 
 
 def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
+    """
+    Make fit curve using fit polynomial coefficients, NumPy's polyval (Links to an external site.), and minimum and
+    maximum x-values
+    :param quadratic_coefficients: ndarray, shape (3,)
+    Quadratic polynomial coefficients, ordered quadratic term first, then linear term, and constant term last.
+    :param min_x: float
+    Starting value for the fit_curve array.
+    :param max_x: float
+    Ending value for the fit curve array.
+    :param number_of_points: int, optional
+    Number of points N to return for final fit curve. Default is 100.
+    :return: fit_curve: ndarray, shape (2, N)
+    x-y data created by the coefficients of the fit function. N is the number of function evaluation points.
+    """
     if max_x < min_x:
         raise ArithmeticError
     if number_of_points <= 2:  # need more than 2 points
--- python/plot_data_with_fit.py
+++ python/plot_data_with_fit.py
@@ -1,8 +1,27 @@
+__author__ = "Jordan Roszko"
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def plot_data_with_fit(data, fit_curve, data_format="o", fit_format=""):
+    """
+    Create a combined scatter and curve plot for the data and the fit polynomial, respectively, using Pyplot's plot
+    (Links to an external site.) function
+    :param data: ndarray, shape(2, M)
+    x-y data that was fit. M is the number of data points.
+    :param fit_curve: ndarray, shape(2, N)
+    x-y data created by the coeffecients of the fit function. N is the number of function evaluation points
+    (usually much greater than M).
+    :param data_format: str, optional
+    Optional formatting specification for the style of the scatter plot data points.  Default is 'o'.  See Pyplot's plot
+    (Links to an external site.) for specifications. (Use Pyplot's plot, not scatter for this.)
+    :param fit_format: tr, optional
+    Optional formatting specification for the curve of the fit function. Default is '' (empty string). See Pyplot's plot
+    (Links to an external site.) for specifications.
+    :return: combined_plot
+    A list of Line2D (Links to an external site.) objects representing the plotted data. This is the default return type
+    from Pyplot's plot.
+    """
     fig, combined_plot = plt.subplots()
     combined_plot.plot(fit_curve[0], fit_curve[1], fit_format)
     combined_plot.plot(data[0], data[1], data_format)
