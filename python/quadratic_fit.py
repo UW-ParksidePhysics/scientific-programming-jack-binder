e0a6dee3ef246e0d4ff1ce10282972debefa6b61
jack-binder <binde009@rangers.uwp.edu>
2022-05-09 15:06:09 -0500

    Updated Project

--- python/quadratic_fit.py
+++ python/quadratic_fit.py
@@ -1,7 +1,16 @@
+__author__ = "Jack Binder"
 import numpy as np
 
 
 def quadratic_fit(data):
+    """
+    Fit a quadratic polynomial to a two row NumPy array of x-y data using NumPy's polyfit (Links to an external site.)
+    function
+    :param data: ndarray, shape (2, M)
+    x-y data to be fit. M is the number of data points.
+    :return:quadratic_coefficients: ndarray, shape (3,)
+    Quadratic polynomial coefficients, ordered quadratic term first, then linear term, and constant term last.
+    """
     xs = data[:, 0]
     ys = data[:, 1]
     quadratic_coefficients = np.polyfit(xs, ys, 2)
