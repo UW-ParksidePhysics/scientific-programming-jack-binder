7844c7af51eff4b1b3c70c28829e8ce89072a721
jack-binder <binde009@rangers.uwp.edu>
2022-05-09 14:25:45 -0500

    Updated Project

--- python/lowest_eigenvectors.py
+++ python/lowest_eigenvectors.py
@@ -16,15 +16,3 @@ if __name__ == "__main__":
     test_values, test_vectors = lowest_eigenvectors(test_matrix, number_of_eigenvectors=2)
     print(test_vectors)
 
-# Module name:
-# lowest_eigenvectors
-# Parameters:
-# square_matrix: ndarray, shape(M, M)
-# Matrix to be characterized. Must be a square matrix of M rows and M columns where M is >=1.
-# number_of_eigenvectors: int, optional
-# Number of eigenvectors K with eigenvalues to return.  Default is 3.
-# Returns:
-# eigenvalues: ndarray, shape(K,)
-# Array of the K lowest-value eigenvalues ordered from lowest to highest.
-# eigenvectors: ndarray, shape(K, M)
-# Array of K eigenvectors with M components arranged in order corresponding to their eigenvalues. The first index should correspond to the eigenvalue index in the eigenvalues array. The order of the components in the eigenvector remains the same as output by NumPy's eig.
